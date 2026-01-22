f69eede2b5f01221897799ecabb5a09343f08a24
tebitan26 <luna.esteban26@gmail.com>
2026-01-22 10:18:41 +0200

    fix(next16): proxy-only (remove middleware.ts) + default export proxy

--- middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-import { proxy, config } from "./proxy";
-
-export { config };
-
-export default function middleware(request: NextRequest) {
-  return proxy(request);
-}
